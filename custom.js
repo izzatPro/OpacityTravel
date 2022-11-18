3bcf101fecd1a17aaab91f2e9aad5d973de38f84
Izzat <superizzat564@gmail.com>
2022-11-18 15:14:18 +0500

    Testimonial section started

--- custom.js
+++ custom.js
@@ -1,10 +1,8 @@
 // Navbar Sticky
-
 // When the user scrolls the page, execute myFunction
 window.onscroll = function(){
     myFunction();
 };
-
 // Get the navbar
 const navbar = document.getElementById("navbar");
 const sticky = navbar.offsetTop;
@@ -17,3 +15,19 @@ function myFunction() {
       navbar.classList.remove("sticky");
     }
   }
+// Smooth Scrolling Start
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){     
+      window.location.hash = hash;
+        });
+      } 
+    });
+  });
+  // Smooth Scrolling End
